4aa2b288ae7ac89ff24d33bf2d84b4361f95db82
Conor Taylor <taylorconor@me.com>
2015-10-08 14:20:33 +0100

    Updating lab1

--- lab1/client.py
+++ lab1/client.py
@@ -1,10 +1,14 @@
+# Lab 1 - Conor Taylor - 12311518
+
 import sys
 import socket
 
 def getResponse(host, port, message):
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	s.connect((host, port))
+	# send the request using the socket
 	s.send("GET /echo.php?message="+message+" HTTP/1.1\r\n\r\n");
+	# recieve up to 1024 bytes from the remote server
 	data = s.recv(1024)
 	s.close()
 	return data
